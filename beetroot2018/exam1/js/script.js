62cc3e28462282db148320f5ec28ca95441a2563
derimedvedmasha <derimedvedmasha@gmail.com>
2018-12-12 12:08:10 +0200

    commit4

--- beetroot2018/exam1/js/script.js
+++ beetroot2018/exam1/js/script.js
@@ -1,4 +1,5 @@
 alert ("hello");
+promt - принимает значение от пользователя
 
 console.log("hello");
 VM193:1 hello
@@ -125,4 +126,165 @@ if (1 > 2) {
 }
 
 add({},2);
+// 
+var a = Math.round(Math.random();
+if (a) {// или (!!a)
+console.log('орел');
+}
+console.log('решка');
+}
+
+
+play() {
+  var a = Math.round(Math.random());
+  console.log(a);
+if (a) {// или (!!a)
+return 'орел';
+}
+return 'решка';
+}
+console.log(play());
+
+// 
+var maxNumber = prompt('Введите максимальное значение ') ;
+var result = Math.floor(Math.random() * maxNumber + 1);
+console.log(result);
+// 
+function random() {
+var maxNumber = +prompt('Введите максимальное значение ') ;
+if (isNaN(maxNumber)) {
+	random();
+	 return; //текущее выполнение ф-и прекратилось
+}
+
+var result = Math.floor(Math.random() * maxNumber + 1);
+console.log(result);
+}
+
+random();
+// 
+function random(repeat) {
+	var str ='Введите максимальное значение';
+	var strRepeat = 'Введите только число!';
+
+if (repeat) {
+	var message = str + '\n' + strRepeat;
+} else {
+	var message = str;
+}
+
+var maxNumber = +prompt(message) ; // !!!!!!!!!!!!!!!!!!!!!!1
+if (isNaN(maxNumber)) {
+	random(true);
+	return;
+}
+
+console.log(Math.floor(Math.random() * maxNumber + 1));
+}
+
+random(false);
+// или сократить так
+
+function random(repeat) {
+	var str ='Введите максимальное значение';
+	var strRepeat = 'Введите только число!';
+
+ var message = (repeat) ? str + '\n' + strRepeat : str; //!!!!!!!!!!!!!!!!!!11
+
+var maxNumber = +prompt(message) ;
+if (isNaN(maxNumber)) {
+	random(true);
+	return;
+}
+
+console.log(Math.floor(Math.random() * maxNumber + 1));
+}
+
+random(false);
+//
+
+моя ф-я
+vvar login = prompt("Введите логин");
+
+
+ 	if (login == null) {
+		alert("Вход отменен")  ;
+    }	;
+
+// 	if (login ! == "Админ") {
+// 		alert("Я вас не знаю!")  ;
+//     }	;
+	
+	if (login == "Админ") {
+// 	 	message = password;
+	var password = prompt("Введите пароль");
+
+		if (password == "Черный Властелин") {
+			alert("Добро пожаловать!");
+		
+		if (password == null) {
+			alert("Вход отменен")  ;
+    	} ;
+
+        } else  {
+ 			alert('Пароль не верен!');
+	}
+		
+	 
+} else  {
+ 	alert('Я вас не знаю!');
+ }
+    
+
+// можно ф-ю вызывать раньше чем она объяывлена 
+
+
+smth();
+
+function smth() {
+	return 1;
+}
+// 
+
+var smth = function hello() {   
+	hello();   yes
+}
+
+hello();		no
+// 
+аргументы
+
+function smth(x, y) {
+	console.log(arguments);
+};
+undefined
+
+smth(1, 2);
+// 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script>
+		var solarSystem = {
+	earth: {
+		population: 7000000000,
+		countries: {
+			ukraine: {
+				population: 48000000,
+			},
+			usa: {
+				population: 150000000,
+			}
+        }
+    }
+};
+console.log("solarSystem");
+	</script>
+</head>
+<body>
+	
+</body>
+</html>
 // 
