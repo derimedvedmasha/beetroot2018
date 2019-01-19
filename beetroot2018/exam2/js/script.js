fd31d255035b68bc1e46d5e04fa9db530650450b
derimedvedmasha <derimedvedmasha@gmail.com>
2019-01-19 16:04:49 +0200

    commit1

---
+++ beetroot2018/exam2/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+			$('.slider').slick({
+				slidesToShow: 3,
+				infinite: false,
+				variableWidth: true,
+				prevArrow: '._prevArrow',
+				nextArrow: '._nextArrow',
+				});
+		});
+
+
+
