43032e96db38431c8a09fe4d0bb6c1754e4b0e3a
derimedvedmasha <derimedvedmasha@gmail.com>
2019-01-25 21:16:16 +0200

    commit2

--- beetroot2018/exam2/js/script.js
+++ beetroot2018/exam2/js/script.js
@@ -1,12 +1,58 @@
 $(document).ready(function(){
-			$('.slider').slick({
+			$('.header_slider').slick({
+				// slidesToShow: 3,
+				// infinite: false,
+				// variableWidth: true,
+				// prevArrow: '._prevArrow',
+				// nextArrow: '._nextArrow',
+				arrows: false,
+				autoplay: true,
+				autoplaySpeed: 2000,
+				});
+		});
+
+$(document).ready(function(){
+			$('.section2_block').slick({
 				slidesToShow: 3,
-				infinite: false,
-				variableWidth: true,
+				// infinite: true,
+				dots: true,
+				// rows: 'slider',
 				prevArrow: '._prevArrow',
 				nextArrow: '._nextArrow',
+				arrows: true,
 				});
 		});
 
 
+$(function(){
+$('.header_arrow').on('click',function(){
+  $('html').animate({
+    scrollTop: $('._scroll_projects').offset().top
+  }, 1000);
+});
+});
+
+$(function(){
+$('.projects').on('click',function(){
+  $('html').animate({
+    scrollTop: $('._scroll_projects').offset().top
+  }, 1000);
+});
+});
+
+$(function(){
+$('.news').on('click',function(){
+  $('html').animate({
+    scrollTop: $('._scroll_news').offset().top
+  }, 1000);
+});
+});
+
+$(function(){
+$('.contact').on('click',function(){
+  $('html').animate({
+    scrollTop: $('._scroll_contact').offset().top
+  }, 1000);
+});
+});
 
